662c5bd0f6e86fb1d63987b522fc7a10ff3f5360
user1 <user1@DESKTOP-LFMHLVK>
2025-01-07 10:05:53 +0900

    Merge branch 'main' of https://github.com/yooseogho/Triple.git into main

--- demo/src/main/java/com/example/demo/config/KafkaConfig.java
+++ demo/src/main/java/com/example/demo/config/KafkaConfig.java
@@ -17,7 +17,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "43.203.124.198:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "54.180.231.89:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
