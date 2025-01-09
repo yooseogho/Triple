961352e74dcc9ee942ae2cc2e2919707b8090346
user1 <user1@DESKTOP-LFMHLVK>
2025-01-09 16:28:28 +0900

    kafka 수정

--- demo/src/main/java/com/example/demo/config/KafkaConfig.java
+++ demo/src/main/java/com/example/demo/config/KafkaConfig.java
@@ -17,7 +17,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "43.201.7.112:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "13.124.108.165:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
--- demo/src/main/java/com/example/demo/service/ProducerService.java
+++ demo/src/main/java/com/example/demo/service/ProducerService.java
@@ -4,16 +4,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
-@Service
+
+@Service // @Service 어노테이션을 사용하여 해당 클래스가 Spring의 서비스 컴포넌트임을 명시합니다.
 public class ProducerService {
 
-    private static final String TOPIC_NAME = "seokho";
+
+    private static final String TOPIC_NAME = "seokho"; // Kafka 토픽 이름을 상수로 선언합니다.
 
     @Autowired
     private KafkaTemplate<String, String> kafkaTemplate;
 
+  
     public void sendMessage(String message) {
-        kafkaTemplate.send(TOPIC_NAME, message);
-        System.out.println("Message sent: " + message);
+
+        kafkaTemplate.send(TOPIC_NAME, message); // KafkaTemplate의 send 메서드를 사용하여 메시지를 특정 토픽에 전송합니다.
+
+     
+        System.out.println("Message sent: " + message); // 메시지가 성공적으로 전송되었음을 콘솔에 출력합니다.
     }
 }
