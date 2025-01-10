e3a4d804a74859863ae3ac735640ba267f3caad1
user1 <user1@DESKTOP-LFMHLVK>
2025-01-10 16:10:12 +0900

    석호

--- demo/src/main/java/com/example/demo/ForwardedHeaderConfig.java
+++ demo/src/main/java/com/example/demo/ForwardedHeaderConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.filter.ForwardedHeaderFilter;
 
+// dsadsadsadsadasd
 @Configuration
 public class ForwardedHeaderConfig {
     @Bean
